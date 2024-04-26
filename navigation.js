b84a491724965af2dd3a49a8c196d846286d2e7b
mimsu24 <154988319+mimsu24@users.noreply.github.com>
2024-04-26 15:00:15 +0300

    Add smooth scrolling to site navigation

---
+++ navigation.js
@@ -0,0 +1,15 @@
+function scrollToCalculator() {
+    document.querySelector('#calculator').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAbout() {
+    document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToServices() {
+    document.querySelector('#services').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToContact() {
+    document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
+}
